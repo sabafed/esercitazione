289956643e2b6f3f85de28c3c3c130bdc8423b66
sabafed_NTTGitKraken <Federico.Saba@emea.nttdata.com>
2023-01-10 16:20:11 +0100

    EsercizioJUnit1

---
+++ workspace/ProgettoJunit/src/main/java/it/corso/junitTest/Main.java
@@ -0,0 +1,17 @@
+package it.corso.junitTest;
+
+import it.corso.junitTest.business.StudentBO;
+import it.corso.junitTest.model.Student;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        StudentBO studentBO = new StudentBO();
+        Student student = studentBO.createStudent("Ale","Sall",29,287566);
+        //studentBO.toString();
+        System.out.println(studentBO.calcolaAnno(student));
+
+    }
+
+}
